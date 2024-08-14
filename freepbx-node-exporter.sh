5c758a7202d10bc3e0dbc440dae9197f380c36e8
Oscar Andres Macias Narvaez <omaciasnarvaez@gmail.com>
2024-08-14 01:18:39 -0500

    ⚡ feat(soap): Add initial commit

---
+++ freepbx-node-exporter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Iniciar Node Exporter
+/usr/local/bin/node_exporter &
+
+# Iniciar Asterisk y FreePBX
+# Comando para iniciar Asterisk y FreePBX
+
+# Mantener el contenedor corriendo
+tail -f /dev/null
